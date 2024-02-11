de32892b32b5870f7c5dfc4c4b5f67a89f213c6e
pasangeek <pasan.madusanka9@gmail.com>
2024-02-11 10:19:40 +0900

    viewpager implementation

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/constant/ConstantNetworkService.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/constant/ConstantNetworkService.kt
@@ -1,5 +1,4 @@
 package com.gtp01.group01.android.recipesmobileapp.constant
-
 /**
  * Object containing constants related to the application's network service, including base URL and endpoints.
  */
@@ -7,8 +6,7 @@ object ConstantNetworkService {
     /**
      * The base URL of the application's API. This URL is used to make network requests.
      */
-    const val BASE_URL = "http://ec2-52-207-150-55.compute-1.amazonaws.com:5000"
-
+    const val  BASE_URL= "http://ec2-52-207-150-55.compute-1.amazonaws.com:5000"
     /**
      * Endpoint for user authorization-related save new user API requests.
      */
