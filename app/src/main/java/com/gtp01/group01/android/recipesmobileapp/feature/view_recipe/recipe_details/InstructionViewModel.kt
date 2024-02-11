b173280196510449f080a1e3235000bef4d884c2
pasangeek <pasan.madusanka9@gmail.com>
2024-02-11 11:53:29 +0900

    viewpager implementation completed

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/view_recipe/recipe_details/InstructionViewModel.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/view_recipe/recipe_details/InstructionViewModel.kt
@@ -1,7 +1,10 @@
 package com.gtp01.group01.android.recipesmobileapp.feature.view_recipe.recipe_details
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class InstructionViewModel : ViewModel() {
+@HiltViewModel
+class InstructionViewModel  @Inject constructor(): ViewModel() {
     // TODO: Implement the ViewModel
 }
