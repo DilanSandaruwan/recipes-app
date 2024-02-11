e1574b47ebe088b5394fa32015f1b4c31362294c
pasangeek <pasan.madusanka9@gmail.com>
2024-02-11 10:20:34 +0900

    viewpager implementation

---
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/view_recipe/recipe_details/IngredientsFragment.kt
@@ -0,0 +1,40 @@
+package com.gtp01.group01.android.recipesmobileapp.feature.view_recipe.recipe_details
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.gtp01.group01.android.recipesmobileapp.R
+import com.gtp01.group01.android.recipesmobileapp.databinding.FragmentIngredientsBinding
+import com.gtp01.group01.android.recipesmobileapp.databinding.FragmentProfileBinding
+import com.gtp01.group01.android.recipesmobileapp.feature.my_profile.ProfileViewModel
+
+class IngredientsFragment : Fragment() {
+    private lateinit var binding: FragmentIngredientsBinding
+    private lateinit var viewModel: IngredientsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Initialize the ViewModel
+        viewModel = ViewModelProvider(this).get(IngredientsViewModel::class.java)
+        binding = FragmentIngredientsBinding.inflate(layoutInflater)
+
+        return binding.root
+
+    }
+
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/view_recipe/recipe_details/ViewPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.gtp01.group01.android.recipesmobileapp.feature.view_recipe.recipe_details
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.gtp01.group01.android.recipesmobileapp.feature.view_recipe.ViewRecipe
+
+class ViewPagerAdapter (fragment:Fragment):FragmentStateAdapter(fragment){
+
+
+    override fun getItemCount(): Int= 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+
+            0->IngredientsFragment()
+            1-> IngredientsFragment()
+            else -> throw IllegalArgumentException("Invalid position $position")
+        }
+    }
+}
