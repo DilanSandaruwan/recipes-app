c5221d0726b30a67fd62aab0f9e7271af6ecc21d
udenisilva <ukui.silva@gmail.com>
2024-02-20 09:06:13 +0530

    merged with Recipe-management epic branch

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/recipe_add_update/RecipeAddFragment.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/recipe_add_update/RecipeAddFragment.kt
@@ -1,31 +1,55 @@
 package com.gtp01.group01.android.recipesmobileapp.feature.recipe_add_update
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.gtp01.group01.android.recipesmobileapp.R
+import com.gtp01.group01.android.recipesmobileapp.databinding.FragmentRecipeAddBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * Fragment for adding a new recipe.
+ */
 @AndroidEntryPoint
 class RecipeAddFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    private var _binding: FragmentRecipeAddBinding? = null
+    private val viewModel: RecipeAddUpdateViewModel by viewModels()
 
-        }
-    }
+    // Lazily initialize binding using the _binding property
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_add, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipe_add, container, false)
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            addRecipeVM = viewModel
+        }
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.apply {
+            // Observe nutrition data changes
+            nutrition.observe(viewLifecycleOwner) {
+                // TODO: Attach calory constraints with saving recipe item
+                Log.e("CHECK", "onViewCreated: ${it[0].name}")
+            }
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        // Nullify the binding to avoid memory leaks
+        _binding = null
     }
 }
