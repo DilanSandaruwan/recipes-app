8a8548353bfca809f2df101fe77c1f5dc080b460
pasangeek <pasan.madusanka9@gmail.com>
2024-03-03 22:17:07 +0900

    image fetch with glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -120,4 +120,7 @@ dependencies {
     implementation(libs.firebase.ui.auth)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.core)
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.13.2")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.13.2")
 }
