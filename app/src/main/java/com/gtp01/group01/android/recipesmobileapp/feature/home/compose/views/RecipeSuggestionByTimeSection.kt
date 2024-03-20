721d62c9003a0e7c3f91e0fd7dbf8a2b275d3b7d
udeni-gtp01 <115888754+udeni-gtp01@users.noreply.github.com>
2024-03-20 19:54:55 +0530

    Revert "UI for showing recipes filtered by a given time in Home screen"

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/home/compose/views/RecipeSuggestionByTimeSection.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/home/compose/views/RecipeSuggestionByTimeSection.kt
@@ -1,133 +1,39 @@
 package com.gtp01.group01.android.recipesmobileapp.feature.home.compose.views
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.gtp01.group01.android.recipesmobileapp.R
-import com.gtp01.group01.android.recipesmobileapp.data.RecipeListTestData
 import com.gtp01.group01.android.recipesmobileapp.shared.model.Recipe
 
 /**
  * Composable function to display the section of recipe suggestions based on time.
  *
  * @param timeBasedRecipeList List<Recipe>: The list of recipes to display in this section. Default is an empty list.
- * @param filterByTime Int: The time duration used for filtering the recipes. Default is 30 min.
- * @param modifier Modifier: The modifier for styling the layout. Default is [Modifier].
  */
 @Composable
-fun RecipeSuggestionByTimeSection(
-    timeBasedRecipeList: List<Recipe> = emptyList(),
-    filterByTime: Int,
-    modifier: Modifier = Modifier
-) {
-    val title = stringResource(R.string.home_suggestion_bytime_title, filterByTime)
-    Column {
-        RecipeSuggestionByTimeTitle(title)
-        RecipeSuggestionByTimeGrid(timeBasedRecipeList)
-    }
-}
-
-/**
- * Composable function to display the grid of recipe suggestions based on time.
- *
- * @param timeBasedRecipeList List<Recipe>: The list of recipes to display in this section.
- * @param modifier Modifier: The modifier for styling the layout. Default is [Modifier].
- */
-@Composable
-fun RecipeSuggestionByTimeGrid(
-    timeBasedRecipeList: List<Recipe>,
-    modifier: Modifier = Modifier
-) {
-    LazyHorizontalGrid(
-        rows = GridCells.Fixed(1),
-        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.content_horizontal_margin)),
-        modifier = modifier
-            .height(dimensionResource(id = R.dimen.large_suggestion_card_view_max_height))
-    ) {
-        items(timeBasedRecipeList) {
-            RecipeByTimeCard(recipe = it)
-        }
-    }
-}
-
-/**
- * Composable function to display a single recipe card.
- *
- * @param recipe Recipe: The recipe to display.
- * @param modifier Modifier: The modifier for styling the layout. Default is [Modifier].
- */
-@Composable
-fun RecipeByTimeCard(
-    recipe: Recipe,
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        color = colorResource(id = R.color.md_theme_surface),
-    ) {
-        Column {
-            Image(
-                painter = painterResource(R.drawable.img_burger_meal_with_french_fries),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = modifier.size(
-                    dimensionResource(id = R.dimen.large_suggestion_card_view_width),
-                    dimensionResource(id = R.dimen.large_suggestion_card_view_img_height)
-                )
-            )
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.Bottom,
-                modifier = modifier
-                    .width(dimensionResource(id = R.dimen.large_suggestion_card_view_width))
-                    .padding(vertical = dimensionResource(id = R.dimen.padding))
-            ) {
-                Text(
-                    text = recipe.recipeName,
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = colorResource(id = R.color.md_theme_onSurface),
-                    modifier = modifier.width(dimensionResource(id = R.dimen.large_suggestion_card_view_name_width))
-                )
-                Row {
-                    Text(
-                        text = stringResource(id = R.string.likes) + " ",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = colorResource(id = R.color.md_theme_outline_mediumContrast)
-                    )
-                    Text(
-                        text = recipe.likeCount.toString(),
-                        style = MaterialTheme.typography.bodySmall,
-                        color = colorResource(id = R.color.md_theme_onSurface),
-                    )
-                }
-            }
-            Row {
-                Text(
-                    text = recipe.preparationTime.toString() + " " + stringResource(id = R.string.min),
-                    style = MaterialTheme.typography.bodySmall,
-                    color = colorResource(id = R.color.md_theme_outline_mediumContrast),
-                    fontWeight = FontWeight.Light,
-                )
+fun RecipeSuggestionByTimeSection(timeBasedRecipeList: List<Recipe> = emptyList()) {
+    RecipeSuggestionByTimeTitle()
+    Column(verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding))) {
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding)),
+            contentPadding = PaddingValues(dimensionResource(R.dimen.padding)),
+            modifier = Modifier
+                .padding(vertical = dimensionResource(R.dimen.padding))
+        ) {
+            items(timeBasedRecipeList) {
+                Text(text = it.recipeName)
             }
         }
     }
@@ -136,20 +42,16 @@ fun RecipeByTimeCard(
 /**
  * Composable function to display the section title for recipe suggestion based on the specified time duration.
  *
- * @param title String: The title to display.
+ * @param timeInMinutes Int: The duration in minutes for the recipe suggestion. Default is 30 minutes.
  */
 @Composable
-fun RecipeSuggestionByTimeTitle(title: String) {
-    Column(
-        modifier = Modifier
-            .height(dimensionResource(id = R.dimen.editText_height))
-            .padding(vertical = dimensionResource(id = R.dimen.padding))
-    ) {
-        Text(
-            text = title,
-            style = MaterialTheme.typography.titleLarge,
-            fontWeight = FontWeight.SemiBold
-        )
+fun RecipeSuggestionByTimeTitle(timeInMinutes: Int = 30) {
+    Column {
+        val context = LocalContext.current
+        val formattedString = remember(timeInMinutes) {
+            context.getString(R.string.home_suggestion_bytime_title, timeInMinutes)
+        }
+        Text(text = formattedString)
     }
 }
 
@@ -159,9 +61,16 @@ fun RecipeSuggestionByTimeTitle(title: String) {
  */
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
-fun PreviewRecipeSuggestionByTimeSection() {
-    val recipeListTestData = RecipeListTestData.getRecipeList()
+fun PreviewRecipeSuggestionTitle() {
     MaterialTheme {
-        RecipeSuggestionByTimeSection(timeBasedRecipeList = recipeListTestData, filterByTime = 30)
+        RecipeSuggestionByTimeTitle()
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun PreviewHomeScreenContent() {
+    MaterialTheme {
+        RecipeSuggestionByTimeSection()
     }
 }
--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/recipe_add_update/RecipeAddFragment.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/recipe_add_update/RecipeAddFragment.kt
@@ -1,55 +1,31 @@
 package com.gtp01.group01.android.recipesmobileapp.feature.recipe_add_update
 
 import android.os.Bundle
-import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.gtp01.group01.android.recipesmobileapp.R
-import com.gtp01.group01.android.recipesmobileapp.databinding.FragmentRecipeAddBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * Fragment for adding a new recipe.
- */
 @AndroidEntryPoint
 class RecipeAddFragment : Fragment() {
-    private var _binding: FragmentRecipeAddBinding? = null
-    private val viewModel: RecipeAddUpdateViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
 
-    // Lazily initialize binding using the _binding property
-    private val binding get() = _binding!!
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
-        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipe_add, container, false)
-        binding.apply {
-            lifecycleOwner = viewLifecycleOwner
-            addRecipeVM = viewModel
-        }
-        return binding.root
+        return inflater.inflate(R.layout.fragment_recipe_add, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.apply {
-            // Observe nutrition data changes
-            nutrition.observe(viewLifecycleOwner) {
-                // TODO: Attach calory constraints with saving recipe item
-                Log.e("CHECK", "onViewCreated: ${it[0].name}")
-            }
-        }
-    }
+    companion object {
 
-    override fun onDestroy() {
-        super.onDestroy()
-        // Nullify the binding to avoid memory leaks
-        _binding = null
     }
 }
