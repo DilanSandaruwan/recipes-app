5886f8148f073fe21fd72966ea377768bbd2ba62
pasangeek <pasan.madusanka9@gmail.com>
2024-03-24 10:00:24 +0900

    add required comments and kdoc

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/shared/sources/Local/LocalDataSource.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/shared/sources/Local/LocalDataSource.kt
@@ -3,11 +3,24 @@ package com.gtp01.group01.android.recipesmobileapp.shared.sources.Local
 import android.content.SharedPreferences
 import javax.inject.Inject
 
-class LocalDataSource@Inject constructor(private val sharePreferences: SharedPreferences) {
-
+/**
+ * A data source class responsible for saving and deleting user-related data in SharedPreferences.
+ *
+ * @property sharedPreferences The SharedPreferences instance used for data storage.
+ */
+class LocalDataSource @Inject constructor(private val sharePreferences: SharedPreferences) {
+    /**
+     * Saves the provided user ID into SharedPreferences.
+     *
+     * @param userId The user ID to be saved.
+     */
     fun saveUserId(userId: Int) {
         sharePreferences.edit().putInt("USERID", userId).apply()
     }
+
+    /**
+     * Deletes the user ID from SharedPreferences.
+     */
     fun deleteUserId() {
         sharePreferences.edit().remove("USERID").apply()
     }
--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/shared/sources/UserIdApiService.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/shared/sources/UserIdApiService.kt
@@ -5,8 +5,16 @@ import com.gtp01.group01.android.recipesmobileapp.shared.model.UserIdResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
-
+/**
+ * Retrofit service interface for fetching user ID based on the provided email.
+ */
 interface UserIdApiService {
+    /**
+     * Retrieves the user ID associated with the provided email address.
+     *
+     * @param userEmail The email address of the user whose ID is to be retrieved.
+     * @return A [UserIdResponse] containing the user ID.
+     */
     @Headers(ConstantNetworkService.ACCEPT_JSON_UTF8)
     @GET(ConstantNetworkService.SEARCH_USER_BY_EMAIL_ENDPOINT)
     suspend fun getUserId(@Path("useremail") userEmail: String): UserIdResponse
