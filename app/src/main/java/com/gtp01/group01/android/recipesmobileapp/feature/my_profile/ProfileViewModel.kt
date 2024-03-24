9109d70770f6499c0edd05b705eb4103bb2d8c81
pasangeek <pasan.madusanka9@gmail.com>
2024-03-24 09:40:15 +0900

    add user id into shared preferences

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/ProfileViewModel.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/ProfileViewModel.kt
@@ -1,21 +1,30 @@
 package com.gtp01.group01.android.recipesmobileapp.feature.my_profile
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.gtp01.group01.android.recipesmobileapp.shared.model.AuthUser
 import com.gtp01.group01.android.recipesmobileapp.feature.my_profile.repository.AuthRepository
+import com.gtp01.group01.android.recipesmobileapp.feature.my_profile.repository.GetUserIdRepository
+import com.gtp01.group01.android.recipesmobileapp.shared.common.ResultState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel  @Inject constructor(private  val authRepository: AuthRepository
+class ProfileViewModel  @Inject constructor(private  val authRepository: AuthRepository, private  val getUserIdRepository: GetUserIdRepository
 ): ViewModel(){
 
     private val _saveUserResult = MutableLiveData<Boolean>()
 
+    private val _userId = MutableLiveData<ResultState>()
+    val saveUserResult: LiveData<Boolean> = _saveUserResult
+    val userId: LiveData<ResultState> = _userId
     /**
      * Save the user details to the backend using the [authRepository].
      * Retrieves the current authenticated user's details from Firebase Authentication
@@ -41,4 +50,24 @@ class ProfileViewModel  @Inject constructor(private  val authRepository: AuthRep
             }
         }
     }
+    /**
+     * Fetches the user ID based on the provided email.
+     */
+
+    fun getUserId (){
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        viewModelScope.launch {
+
+            try {
+            val email= currentUser?.email.orEmpty()
+            val result = getUserIdRepository.getUserId(email)
+            _userId.value = ResultState.Success(result)
+                // Log the user ID
+                Log.d("ProfileViewModel", "User ID: $result")
+            }catch (e: Exception) {
+                _userId.value = ResultState.Failure("Failed to fetch user ID")
+            }
+        }
+    }
+
 }
