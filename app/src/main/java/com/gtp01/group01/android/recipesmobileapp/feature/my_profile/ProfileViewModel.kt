ae8bc4748246e513a5ea7388b081239add86fae3
pasangeek <pasan.madusanka9@gmail.com>
2024-03-25 21:00:44 +0900

    code clean up ProfileViewModel

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/ProfileViewModel.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/ProfileViewModel.kt
@@ -1,20 +1,12 @@
 package com.gtp01.group01.android.recipesmobileapp.feature.my_profile
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.google.firebase.auth.FirebaseAuth
-import com.gtp01.group01.android.recipesmobileapp.shared.model.AuthUser
 import com.gtp01.group01.android.recipesmobileapp.feature.my_profile.repository.AuthRepository
 import com.gtp01.group01.android.recipesmobileapp.feature.my_profile.repository.GetUserIdRepository
 import com.gtp01.group01.android.recipesmobileapp.shared.common.ResultState
 import com.gtp01.group01.android.recipesmobileapp.shared.sources.Local.LocalDataSource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
