a1cd3812fc1b1af3dfb1ff851c0239c40a3ea59f
pasangeek <pasan.madusanka9@gmail.com>
2024-03-30 11:50:24 +0900

    edit profile view added

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/ProfileFragment.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/ProfileFragment.kt
@@ -5,12 +5,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.gtp01.group01.android.recipesmobileapp.R
 import com.gtp01.group01.android.recipesmobileapp.constant.AuthProviders
 import com.gtp01.group01.android.recipesmobileapp.constant.AuthUtils
 import com.gtp01.group01.android.recipesmobileapp.constant.ConstantRequestCode.MY_REQUEST_CODE
@@ -18,6 +22,7 @@ import com.gtp01.group01.android.recipesmobileapp.databinding.FragmentProfileBin
 import com.gtp01.group01.android.recipesmobileapp.shared.sources.Local.LocalDataSource
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 /**
  * Fragment to display and manage user profile information.
@@ -39,9 +44,25 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment using view binding
+
         viewModel = ViewModelProvider(requireActivity())[ProfileViewModel::class.java]
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // Find the TextView
+        val tvEditProfile = requireView().findViewById<TextView>(R.id.tvEditProfile)
+
+        // Set OnClickListener for tvNotification
+        tvEditProfile.setOnClickListener {
+            // Navigate back to the previous fragment
+            findNavController().navigate(R.id.action_profileFragment_to_editProfileFragment)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,6 +71,7 @@ class ProfileFragment : Fragment() {
         val providers = AuthProviders.providers
 
         viewUserDetails()
+        binding.tvNotification
         // Sign-out from Firebase Authentication
         binding.btnLogout.setOnClickListener {
             // Remove user ID from SharedPreferences
