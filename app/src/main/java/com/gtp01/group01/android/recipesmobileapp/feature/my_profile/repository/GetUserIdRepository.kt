6beec59e570216c26dedfb449ebbc9be9fed9b9b
pasangeek <pasan.madusanka9@gmail.com>
2024-03-31 00:12:08 +0900

    authuser data class remove due to duplicate class user ,and also user id response . and code clean up according to reviews

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/repository/GetUserIdRepository.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_profile/repository/GetUserIdRepository.kt
@@ -1,13 +1,12 @@
 package com.gtp01.group01.android.recipesmobileapp.feature.my_profile.repository
 
-import com.gtp01.group01.android.recipesmobileapp.shared.model.UserIdResponse
-import com.gtp01.group01.android.recipesmobileapp.shared.sources.AuthApiService
+import com.gtp01.group01.android.recipesmobileapp.shared.model.User
 import com.gtp01.group01.android.recipesmobileapp.shared.sources.UserIdApiService
 import javax.inject.Inject
 
-class GetUserIdRepository @Inject constructor(private val userIdApiService: UserIdApiService){
+class GetUserIdRepository @Inject constructor(private val userIdApiService: UserIdApiService) {
 
-    suspend fun getUserId(userEmail: String): UserIdResponse {
+    suspend fun getUserId(userEmail: String): User {
         return userIdApiService.getUserId(userEmail)
     }
 }
