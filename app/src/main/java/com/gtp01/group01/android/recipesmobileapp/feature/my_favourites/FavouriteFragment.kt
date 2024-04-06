eddaef758ac420587bc5409b510c2a8a208acef4
tharanga105 <tsrajapaksha105@gmail.com>
2024-04-06 21:23:52 +0530

    Updated files

--- app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_favourites/FavouriteFragment.kt
+++ app/src/main/java/com/gtp01/group01/android/recipesmobileapp/feature/my_favourites/FavouriteFragment.kt
@@ -5,8 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.gtp01.group01.android.recipesmobileapp.databinding.FragmentFavouriteBinding
 import com.gtp01.group01.android.recipesmobileapp.shared.model.Recipe
@@ -15,7 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class FavouriteFragment : Fragment(), FavoritesAdapter.OnItemClickListener {
     private lateinit var binding: FragmentFavouriteBinding
-    private val viewModel: FavoritesViewModel by viewModels()
+    private lateinit var viewModel: FavoritesViewModel
 
     private lateinit var adapter: FavoritesAdapter
 
@@ -29,10 +29,10 @@ class FavouriteFragment : Fragment(), FavoritesAdapter.OnItemClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        viewModel =  ViewModelProvider(this).get(FavoritesViewModel::class.java)
         adapter = FavoritesAdapter(emptyList(), this)
-        binding.recyclerViewFavorites.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerViewFavorites.adapter = adapter
+        binding.recyclerFavourites.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerFavourites.adapter = adapter
 
         viewModel.favoriteRecipes.observe(viewLifecycleOwner, Observer { favorites ->
             adapter.favoriteRecipes = favorites
@@ -40,10 +40,10 @@ class FavouriteFragment : Fragment(), FavoritesAdapter.OnItemClickListener {
         })
 
         // Fetch favorite recipes
-        viewModel.fetchFavoriteRecipes()
+        viewModel.fetchFavoriteRecipes(userId = 10)
     }
 
     override fun onItemClick(recipe: Recipe) {
         // Handle item click
-    }
+        }
 }
